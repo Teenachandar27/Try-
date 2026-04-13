02964708324bf45eee3989a745b89a6db74c0dc8
Teena2702 <teemachandar02@gmail.com>
2026-04-13 07:42:35 +0530

    Fixed folder scope

---
+++ AppTest.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        // This test will always pass. 
+        // In a real project, you'd test your App.java logic here.
+        assertTrue( true );
+    }
+}
